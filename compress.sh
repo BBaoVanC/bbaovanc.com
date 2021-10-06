842e101948a82125ea5138d56b02bc1b4ac59204
BBaoVanC <bbaovanc@bbaovanc.com>
2021-10-05 19:12:32 -0500

    Update bobatheme with icons

--- compress.sh
+++ compress.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-files=$(find public/ -name "*.html" -o -name "*.xml" -o -name "*.css" -o -name "*.js")
+files=$(find public/ -name "*.html" -o -name "*.xml" -o -name "*.css" -o -name "*.js" -o -name "*.svg")
 
 printf '%s\n' "$files" | parallel $@ "echo 'GZIP {}' && gzip -fnk {}"
 printf '%s\n' "$files" | parallel $@ "echo 'BROTLI {}' && brotli -fZk {}"
