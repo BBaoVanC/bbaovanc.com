b2d3bd99f31f6fa0e319ee9f50e1ae0549bee768
BBaoVanC <bbaovanc@bbaovanc.com>
2021-11-23 18:44:57 -0600

    Enable missing translation placeholder in demo.sh [skip ci]

--- demo.sh
+++ demo.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -x
 rm -rf public/
-hugo -D --baseURL "https://demo.bbaovanc.com/test/$1"
+HUGO_ENABLEMISSINGTRANSLATIONPLACEHOLDERS=true hugo -D --baseURL "https://demo.bbaovanc.com/test/$1"
 rsync -atvPzc --delete public/ bbaosrv:/var/www/demo.bbaovanc.com/test/$1/
