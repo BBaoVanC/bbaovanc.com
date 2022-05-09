338746e1f370ee06228b3c03abea8f101f4d37b7
BBaoVanC <bbaovanc@bbaovanc.com>
2022-05-08 20:06:40 -0500

    Add stuff

---
+++ changed-urls.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+diff -qrN old/ new/ |
+    grep -oP '(?<=\s)new\/\S*' |
+    xargs -d '\n' ls -1d 2>/dev/null |
+    grep 'index.html$' |
+    sed 's/index.html$//' |
+    sed "s/^new/${SCHEME:=http}:\/\/${DOMAIN:=example.com}/"
