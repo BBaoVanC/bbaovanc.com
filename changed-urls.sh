14565ea34328f250239b1bc82f3e128b9b4c8c29
BBaoVanC <bbaovanc@bbaovanc.com>
2023-04-15 19:27:00 -0500

    Remove leftover files for Lighthouse workflow [skip ci]
    Leftover after removal in 1574cd8515eca3696d948da006a3028d6295ba5b

--- changed-urls.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-diff -qrNtbB old/ new/ |
-    grep -oP '(?<=\s)new\/\S*' |
-    xargs -d '\n' ls -1d 2>/dev/null |
-    grep 'index.html$' |
-    sed 's/index.html$//' |
-    sed "s/^new/${SCHEME:=http}:\/\/${DOMAIN:=example.com}/"
