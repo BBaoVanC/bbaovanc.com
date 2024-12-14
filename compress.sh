e80970da7f29f74857cd1dc894ccff142da35edf
BBaoVanC <bbaovanc@bbaovanc.com>
2024-12-13 23:06:44 -0600

    Add zstd compression

--- compress.sh
+++ compress.sh
@@ -4,3 +4,6 @@ files=$(find public/ -name "*.html" -o -name "*.xml" -o -name "*.css" -o -name "
 
 printf '%s\n' "$files" | parallel $@ "echo 'GZIP {}' && gzip -fnk {}"
 printf '%s\n' "$files" | parallel $@ "echo 'BROTLI {}' && brotli -fZk {}"
+# level 9 chosen based on `zstd -b1 -e22 public/blog/swapfile-guide/index.html`
+# gives a slightly better ratio than default (3)
+printf '%s\n' "$files" | parallel $@ "echo 'ZSTD {}' && zstd -f -9 {}"
