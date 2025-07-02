e91a62ee1848f092c36a320b827ae16c1620b6b0
BBaoVanC <bbaovanc@bbaovanc.com>
2025-07-02 02:14:42 -0500

    Add search page using Pagefind
    Updates #79. Still a bit rough but it works.

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
-#!/bin/sh -ex
+#!/bin/bash -ex
+# POSIX sh doesn't have the time command
 
 [ -z "$DEST" ] && DEST="public/"
 rm -rf "$DEST"
 
 hugo --printI18nWarnings --printPathWarnings -d "$DEST" $@
+
+if [ -z "$NO_SEARCH" ]; then
+    time pagefind --site "$DEST"
+fi
 
 if [ -z "$NO_COMPRESS" ]; then
     export DEST
---
+++ content/search/index.md
@@ -0,0 +1,12 @@
+---
+
+title: Search
+layout: search
+menus:
+  main:
+    name: Search
+
+---
+
+Quickly search the full text of any article on my website! Powered by
+[Pagefind](https://pagefind.app/).
