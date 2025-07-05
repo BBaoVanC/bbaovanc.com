9c85a67e224a572aca7bfc17ca9790b454b96fb3
BBaoVanC <bbaovanc@bbaovanc.com>
2025-07-05 01:33:04 -0500

    Prevent accidentally wiping the entire demo or staging environment [skip ci]

--- demo.sh
+++ demo.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -ex
+[ -z "$1" ] && echo "Please give a demo name" && exit 1
 ./audit.sh
 DEST=public.demo/ ./build.sh --baseURL "https://demo.bbaovanc.com/test/$1"
 rsync -avPzc --delete-delay public.demo/ bbaosrv:/var/www/demo.bbaovanc.com/test/$1/
--- staging.sh
+++ staging.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -ex
+[ -z "$1" ] && echo "Please give a demo name" && exit 1
 ./audit.sh
 DEST=public.staging/ ./build.sh --baseURL "https://demo.bbaovanc.com/staging/$1"
 rsync -avPzc --delete-delay public.staging/ bbaosrv:/var/www/demo.bbaovanc.com/staging/$1/
